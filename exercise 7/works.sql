35a66d0baf39ffb6650d6f6f40a436318d16362a
yossefkatri <45340933+yossefkatri@users.noreply.github.com>
2020-07-24 15:25:58 +0300

    Update works.sql

--- exercise 7/works.sql
+++ exercise 7/works.sql
@@ -1,8 +1,8 @@
 create table Works
-{
+(
 IDVolunteer NUMERIC(9) Not null,
 IDScientist INT not null,
 primary key(IDVolunteer),
 foreign key(IDVolunteer) references volunteer(id),
-foreign key(IDScientist) references Scientist(scientistId)
-};
+foreign key(IDScientist) references asebbah.Scientist(scientistId)
+);
